4ed9741379e540a8c91f2e3da2b85cbde2290e9d
DKSC1 <dawidkrzeminski5@gmail.com>
2025-01-20 15:08:44 +0100

    Initial commit

---
+++ zad 2 list/zad 2 list/zad_2_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  zad_2_listApp.swift
+//  zad 2 list
+//
+//  Created by user270910 on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct zad_2_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zad 2 list/zad 2 listTests/zad_2_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  zad_2_listTests.swift
+//  zad 2 listTests
+//
+//  Created by user270910 on 1/20/25.
+//
+
+import Testing
+@testable import zad_2_list
+
+struct zad_2_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
